3f6e561216d3da8193d26cdb5ed16bcaa256fc69
sashapavlu <samovar2003@mail.ru>
2017-03-29 01:33:33 +0300

    новый класс HorizontalLine

---
+++ Snake/Snake/HorizontalLine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class HorizontalLine
+    {
+        List<Point> plist;
+
+        public HorizontalLine(int xLeft, int xReight, int y, char sym)
+        {
+            plist = new List<Point>();
+            for (int x = xLeft; x <= xReight; x++)
+            {
+                Point p = new Point(x, y, sym);
+                plist.Add(p);
+            }
+        }
+        public void Draw ()
+        {
+            foreach(Point p in plist)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -16,25 +16,8 @@ namespace Snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            List<int> numlist = new List<int>();
-            numlist.Add(0);
-            numlist.Add(1);
-            numlist.Add(2);
-
-            int x = numlist[0];
-            int y = numlist[1];
-            int z = numlist[2];
-
-            foreach(int i in numlist)
-            {
-                Console.WriteLine(i);
-            }
-
-            numlist.RemoveAt(0);
-
-            List<Point> plist = new List<Point>();
-            plist.Add(p1);
-            plist.Add(p2);
+            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
+            line.Draw();
 
             Console.ReadLine();
         }
